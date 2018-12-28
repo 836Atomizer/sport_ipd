5e63ebf7e8e0ae85fb3ff08f9ba37d8264894b4d
836Atomizer <44051367+836Atomizer@users.noreply.github.com>
2018-12-27 23:47:26 -0500

    Add files via upload

---
+++ include/head.php
@@ -0,0 +1,7 @@
+ <meta charset="UTF-8">
+     
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/body.css">
+        <script src="../js/jquery.min.js"></script>
+    <link rel="icon" href="../img/ipd.ico">
